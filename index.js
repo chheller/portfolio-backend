21feacab5a49f823e901b0aea0fd66e2250ef55b
Charles <charles@supplypike.com>
2019-08-29 01:07:40 -0500

    Fix modules

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 module.exports = (req, res) => {
+  if (req.method !== "POST") return res.send("Method Not Allowed.");
   try {
-    if (req.method !== "POST") return res.send("Method Not Allowed.");
     const { responseToken } = JSON.parse(req.body);
     return axios
       .post(
